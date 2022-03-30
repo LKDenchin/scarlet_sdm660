1a38f141a27414106a6244e7c0ac5511e2e25d8c
Daniel Rosenberg <drosen@google.com>
2022-03-30 15:19:00 -0700

    BACKPORT: ANDROID: fuse-bpf: Use fuse_bpf_args in uapi
    fuse_args is not suitable for use in the uapi - it is not stable, and
    contains internal pointers. Replace with stable equivalent.
    
    The end_offset values are currently unused and unset, but will be used
    in a follow up patch by the verifier.
    
    Test: fuse_test, atest ScopedStorageDeviceTest pass
    Bug: 202785178
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: Ic1c12f9706aeae233cc30a0d68ed2533030e485b
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- include/linux/bpf_types.h
+++ include/linux/bpf_types.h
@@ -36,7 +36,7 @@ BPF_PROG_TYPE(BPF_PROG_TYPE_LIRC_MODE2, lirc_mode2)
 BPF_PROG_TYPE(BPF_PROG_TYPE_SK_REUSEPORT, sk_reuseport)
 #endif
 #ifdef CONFIG_FUSE_BPF
-BPF_PROG_TYPE(BPF_PROG_TYPE_FUSE, fuse, struct fuse_args, struct fuse_args)
+BPF_PROG_TYPE(BPF_PROG_TYPE_FUSE, fuse, struct fuse_bpf_args, struct fuse_bpf_args)
 #endif
 
 BPF_PROG_TYPE(BPF_PROG_TYPE_FLOW_DISSECTOR, flow_dissector)
