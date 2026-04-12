6f5d6535ae2563ce0dff735301df9503e0b99552
Santhosh <santhosh.user.why.red@gmail.com>
2026-04-12 12:58:46 +0530

    selinux: bpf: Fix bpf_fd_pass() to check receiver credentials for CAP_SYS_ADMIN
    bpf_fd_pass() is called from two sites:
    
    	file_has_perm()              -> bpf_fd_pass(file, cred_sid(cred), cred)
    	selinux_binder_transfer_file -> bpf_fd_pass(file, sid, to)
    
    In both cases the sid argument represents the RECEIVING process. The
    previous cap_capable(current_cred()) check was checking the SENDING
    process credentials instead, since current during a binder transaction
    is the sender not the receiver.
    
    Fix by threading the receiver cred through bpf_fd_pass() as a new
    parameter and passing it to cap_capable() instead of current_cred().
    The receiver cred is already available at both call sites:
    	file_has_perm() has cred in scope
    	selinux_binder_transfer_file() has to as a parameter
    
    This makes the CAP_SYS_ADMIN bypass semantically correct: we check
    the credentials of the process that will actually use the fd, which
    is consistent with what sid represents in both call sites.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1905,7 +1905,7 @@ static inline int file_path_has_perm(const struct cred *cred,
 }
 
 #ifdef CONFIG_BPF_SYSCALL
-static int bpf_fd_pass(struct file *file, u32 sid);
+static int bpf_fd_pass(struct file *file, u32 sid, const struct cred *cred);
 #endif
 
 /* Check whether a task can use an open file descriptor to
@@ -1940,7 +1940,7 @@ static int file_has_perm(const struct cred *cred,
 	}
 
 #ifdef CONFIG_BPF_SYSCALL
-	rc = bpf_fd_pass(file, cred_sid(cred));
+	rc = bpf_fd_pass(file, cred_sid(cred), cred);
 	if (rc)
 		return rc;
 #endif
@@ -2278,7 +2278,7 @@ static int selinux_binder_transfer_file(const struct cred *from,
 	}
 
 #ifdef CONFIG_BPF_SYSCALL
-	rc = bpf_fd_pass(file, sid);
+	rc = bpf_fd_pass(file, sid, to);
 	if (rc)
 		return rc;
 #endif
@@ -6938,7 +6938,7 @@ static u32 bpf_map_fmode_to_av(fmode_t fmode)
  * access the bpf object and that's why we have to add this additional check in
  * selinux_file_receive and selinux_binder_transfer_files.
  */
-static int bpf_fd_pass(struct file *file, u32 sid)
+static int bpf_fd_pass(struct file *file, u32 sid, const struct cred *cred)
 {
 	struct bpf_security_struct *bpfsec;
 	struct bpf_prog *prog;
